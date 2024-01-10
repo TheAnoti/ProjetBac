1be7f0b9b43a71e4d981ec9a2e84e3ea2aa12b1a
TheAnoti <p.matthieu@live.fr>
2024-01-10 11:59:59 +0100

    Modif du style

--- controller/book.php
+++ controller/book.php
@@ -2,20 +2,21 @@
         
 ?>
 
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Silence on lit</title>
-    <link rel= "stylesheet" href="../style/style.css">
+    <title>Bibliothèque</title>
+    <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
-    <?php
+     <?php
         require_once('../view/modules/header.php');
         require_once('../view/modules/nav.php');
-    ?>
+     ?>
+
+     
     <main>
 
     </main>
--- view/modules/header.php
+++ view/modules/header.php
@@ -1,8 +1,8 @@
-<head>
+<header>
         <h1>
             Silence ont lit !
         </h1>
         <h3>
             Ne parlez pas comme Alex, faites juste silence, n'est-ce pas Alex.
         </h3>
-    </head>
+    </header>
